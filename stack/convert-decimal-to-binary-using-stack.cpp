80b4b0464e79e3f0086072d2752cb298648d941b
Masterplan <76619967+reprogamaco@users.noreply.github.com>
2024-02-19 23:35:39 +0300

    convert decimal number to binary number - stack
    Using the stacks to convert the decimal input of the user to convert it to binary number.

--- stack/convert-decimal-to-binary-using-stack.cpp
+++ stack/convert-decimal-to-binary-using-stack.cpp
@@ -1,4 +1,4 @@
-// This is the program that convert decimal to binary using stack
+// A program converting decimal to binary to stack
 
 #include <iostream>
 
@@ -19,29 +19,20 @@ class Stack{
                 exit(0);
             }
 
-            top=top+1;
-            array[top]=value; // I know i can write array[top++]=value;
-            std::cout<<value<<" was pushed to stack!"<<std::endl;
+            // push logic
+            top=top+1; // top++;
+            array[top]=value;
         }
 
-        void pop(){
+        void pop(int value){
             if(isEmpty()){
                 std::cout<<"Stack underflow!"<<std::endl;
                 exit(0);
             }
 
-            top=top-1;
-            std::cout<<array[top+1]<<" was popped out of stack!"<<std::endl;
+            // pop up logic
+            top=top-1; // top--;
         }
-
-        int Top(){
-            if(isEmpty()){
-                std::cout<<"The stack is empty!"<<std::endl;
-                exit(0);
-            }
-            return array[top];
-        }
-
 
         bool isEmpty(){
             if(top==-1){
@@ -51,15 +42,48 @@ class Stack{
             }
         }
 
+        void show(){
+            if(isEmpty()){
+                std::cout<<"Nothing to show!"<<std::endl;
+                exit(0);
+            }
+
+            int copy=top;
+            while(copy>-1){
+                std::cout<<array[copy];
+                copy=copy-1; // --copy;
+            }
+        }
 };
 
-int main(){
-    int decimal;
 
-    std::cout<<"\t\t  THE PROGRAM TO CONVERT DECIMAL TO BINARY"<<std::endl;
-    std::cout<<"\t Enter a number: ";
+int main(){
+    Stack stack;
+    int decimal;
+    bool status=true;
+
+    std::cout<<"Enter a number: ";
     std::cin>>decimal;
 
+    while(status){
+        if(decimal==1){
+            stack.push(1);
+            std::cout<<"Pushed the last 1"<<std::endl;
+            break;
+        }
+        if(decimal%2==0){
+            decimal=(decimal-(decimal%2))/2; // decimal = ( decimal )/2;
+            stack.push(0);
+            std::cout<<"Pushed 0"<<std::endl;
+        }else{
+            decimal=(decimal-(decimal%2))/2;
+            stack.push(1);
+            std::cout<<"Pushed 0"<<std::endl;
+        }
+    }
+    
+
+    stack.show();
     
     return 0;
 }
